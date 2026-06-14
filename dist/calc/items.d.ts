08219c4c753f9036c034680592326d4d983b27ee
ViZarSmogon <vizarsmogon@gmail.com>
2026-06-14 16:15:47 +0200

    push

---
+++ dist/calc/items.d.ts
@@ -0,0 +1,13 @@
+import type { Generation, TypeName, StatID } from './data/interface';
+export declare const SEED_BOOSTED_STAT: {
+    [item: string]: StatID;
+};
+export declare function getItemBoostType(item: string | undefined): "Normal" | "Fighting" | "Flying" | "Poison" | "Ground" | "Rock" | "Bug" | "Ghost" | "Steel" | "Fire" | "Water" | "Grass" | "Electric" | "Psychic" | "Ice" | "Dragon" | "Dark" | "Fairy" | undefined;
+export declare function getBerryResistType(berry: string | undefined): "Normal" | "Fighting" | "Flying" | "Poison" | "Ground" | "Rock" | "Bug" | "Ghost" | "Steel" | "Fire" | "Water" | "Grass" | "Electric" | "Psychic" | "Ice" | "Dragon" | "Dark" | "Fairy" | undefined;
+export declare function getFlingPower(item?: string): 0 | 100 | 10 | 80 | 90 | 20 | 50 | 30 | 60 | 85 | 110 | 40 | 130 | 95 | 70;
+export declare function getNaturalGift(gen: Generation, item: string): {
+    t: TypeName;
+    p: number;
+};
+export declare function getTechnoBlast(item: string): "Fire" | "Water" | "Electric" | "Ice" | undefined;
+export declare function getMultiAttack(item: string): TypeName | undefined;
